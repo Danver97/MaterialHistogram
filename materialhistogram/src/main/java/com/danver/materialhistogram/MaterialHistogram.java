fbcf82691874a0e8538bfb46393f391ab48fdb86
Christian Paesante <chri.pae@gmail.com>
2017-06-01 23:14:18 +0200

    Small fixes and code improvements from Android Studio.

--- materialhistogram/src/main/java/com/danver/materialhistogram/MaterialHistogram.java
+++ materialhistogram/src/main/java/com/danver/materialhistogram/MaterialHistogram.java
@@ -11,7 +11,6 @@ import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
